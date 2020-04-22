211c39c10125a19e9ba6e2c2e59336e235282869
HSEc0der <55021869+HSEc0der@users.noreply.github.com>
2020-04-23 02:12:35 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,75 +1,86 @@
-#include "sort.h"
+#include "Header.h"
 #include <iostream>
 #include <string>
 #include <stdlib.h>
 #include <vector>
 
-using namespace std;
 // конструктор
-N::our_class::our_class(const string& str) {
-	size_t size = str.size(); // длина подаваемой на вход строки
-	int i_left = 0; 
-	int num_space = 5; // число пробелов
-	int i_right;
-	vector<int> v;
+N::our_class::our_class(const std::string &str) {
+	std::vector<std::string> v;
+	int size = str.size();
+	int left = 0;
+	int right;
+	int numspace = 5;
 	for (int i = 0; i < size; i++) {
-		if (str[i] == ' ' && num_space > 0) {
-			i_right = i;
-			v.push_back(stoi(str.substr(i_left, i_right)));
-			i_left = i + 1;
-			num_space--;
+		if (str[i] == ' ' && numspace > 0) {
+			right = i;
+			v.push_back(str.substr(left, right - left));
+			left = i + 1;
+			numspace--;
 		}
 	}
-	home = v[0];
-	flat = v[1];
-	s = v[3];
-	fio = str.substr(i_left, size);
+	home = std::stoi(v[0]);
+	flat = std::stoi(v[1]);
+	s = std::stoi(v[3]);
+	fio = str.substr(left);
 	all_string = str;
 }
 
-
-// конструктор по умолчанию
 N::our_class::our_class() {
 	home = 0;
 	flat = 0;
 	s = 0;
 	fio = "";
 	all_string = "";
-};
+}
 
+//возвращает номер дома
+int N::our_class::Home() {
+	return home;
+}
+//возвращает номер квартиры
+int N::our_class::Flat() {
+	return flat;
+}
 
-// функция, работает так же, как и конструктор
-void N::our_class::set_all(string& str) {
-	size_t size = str.size(); // длина подаваемой на вход строки
-	int i_left = 0;
-	int num_space = 5; // число пробелов
-	int i_right;
-	vector<string> v;
+//возвращает площадь
+int N::our_class::S() {
+	return s;
+}
+
+//возврашает ФИО
+std::string N::our_class::Fio() {
+	return fio;
+}
+
+//возвращает всю строку
+std::string N::our_class::AllString() {
+	return all_string;
+}
+
+//работает также, как конструктор
+void N::our_class::set_all(const std::string& str) {
+	std::vector<std::string> v;
+	int size = str.size();
+	int left = 0;
+	int right;
+	int numspace = 5;
 	for (int i = 0; i < size; i++) {
-		if (str[i] == ' ' && num_space > 0) {
-			i_right = i;
-			v.push_back(str.substr(i_left, i_right));
-			i_left = i + 1;
-			num_space--;
+		if (str[i] == ' ' && numspace > 0) {
+			right = i;
+			v.push_back(str.substr(left, right - left));
+			left = i + 1;
+			numspace--;
 		}
 	}
-	home = stoi(v[0]);
-	flat = stoi(v[1]);
-	//room = v[2];
-	s = stoi(v[3]);
-	fio = str.substr(i_left, size);
+	home = std::stoi(v[0]);
+	flat = std::stoi(v[1]);
+	s = std::stoi(v[3]);
+	fio = str.substr(left);
 	all_string = str;
 }
 
-// показывает строку, которая была на входе
-string N::our_class::showAll() {
-	return all_string;
-}
-string N::our_class::showSorted() {
-	return to_string(home) + " " + to_string(flat) + " " + to_string(s) + " " + fio;
-}
- 
-// перегрузка оператора сравнения БОЛЬШЕ
+//// перегрузка оператора сравнения БОЛЬШЕ
 bool N::operator>(const our_class& left, const our_class& right) {
 	if ((left.s < right.s) || (left.s == right.s && left.home > right.home) || (left.s == right.s && left.home == right.home && left.flat > right.flat)
 		|| (left.s == right.s && left.home == right.home && left.flat == right.flat && left.fio > right.fio)) {
@@ -78,8 +89,7 @@ bool N::operator>(const our_class& left, const our_class& right) {
 	}
 	else
 		return false;
-};
-
+}
 
 // перегрузка оператора сравнения БОЛЬШЕ ИЛИ РАВНО
 bool N::operator>=(const our_class& left, const our_class& right) {
