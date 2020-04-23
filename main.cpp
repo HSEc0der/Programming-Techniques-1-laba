cc6f8973232b21d4f7b24a283caeea9983ad4073
HSEc0der <55021869+HSEc0der@users.noreply.github.com>
2020-04-23 14:00:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,65 @@
 #include "QuickSort.h"
 #include "Vstavka.h"
 #include "Bubble.h"
+#include <cstdio>
 
+void sort(std::vector<std::string> &mas, int length, int which_sort, int show, std::string fileName) {
+	// which_sort == 0 -> cортировка пузырьком
+	//which_sort == 1 -> сортировка вставками
+	//which_sort == 2 -> быстрая сортировка
+	// mas - источник данных
+	//show - cколько даннных нужно вывести
+	//fileName - имя файла, куду нужно записать отсортированный массив, если пустая строка - то ничего не нужно делать.
+	N::our_class* cls = new N::our_class[length]; // динамический массив
+	for (int i = 0; i < length; i++) {
+		cls[i].set_all(mas[i]);	
+	}
+	switch (which_sort) {
+		case 0:
+			{
+			bubble(cls, length);
+			break;
+			}
+		case 1:
+		{
+			vstavka(cls, length);
+			break;
+		}
+		case 2:
+		{
+			quicksort(cls, length);
+			break;
+		}
+		default:
+			std::cout << "Вы ввели не то число\n";
+	}
+	for (int i = 0; i < show; i++) {
+		std::cout << cls[i].showSorted() << "\n";
+	}
+	// если передана непустая строка, то нужно записывать данные в файл
+	if (!(fileName == "")) {
+		std::fstream file;
+		file.open(fileName, std::fstream::in | std::fstream::out);
+		
+		// проверяем, корректное ли имя файла
+		if (file.is_open()) {
+			for (int j = 0; j < length; j++) {
+				file << cls[j].showSorted() << "\n";
+			}
+		}
+		else {
+			// запись в файл
+			std::cout << "Вы ввели неправильное имя файла\n";
+			return;
+		}
+
+	}
+	else {
+		std::cout << "Ничего записывать в файл не надо.\n";
+	}
+	delete[] cls;
+
+}
 
 int main() {
 	setlocale(LC_ALL, "Russian");
@@ -17,35 +75,14 @@ int main() {
 	std::vector<std::string> Data;
 	std::ifstream In("D:\\УЧЕБА\\Методы программирования\\19var\\DataCpp.txt");
 	std::string str;
-	std::string s;
 	while (!In.eof())
 	{
 		getline(In, str);
 		Data.push_back(str);
 	}
+	///////////////////////////////
 
-	int n = Data.size(); // количество строк в файле
-	std::cout << n << "\n";
-	int sep[9] = { 100, 500,1000,10000, 20000, 40000, 50000, 70000, n };
-	//int sep[1] = { 100 };
-	int length;
-
-
-	for (int k = 0; k < 9; k++) {
-		length = sep[k];
-		N::our_class* cls = new N::our_class[length]; // динамический массив
-		for (int i = 0; i < length; i++) {
-			cls[i].set_all(Data[i]);
-
-		}
-
-		float start_time = clock(); // начало отсчета времени
-
-		//bubble(cls, length);
-		//vstavka(cls, length);
-		//quicksort(cls, 0, length - 1);
-		float end_time = clock(); // конец отсчета времени
-		delete[] cls;
-		std::cout << "Для n = " << sep[k] << " время выполнения: " << (end_time - start_time) / CLOCKS_PER_SEC << "\n";
-	}
+	std::string s; // полное имя файла, куда будет записан отсортированный массив
+	sort(Data, 100, 2, 100,"");
+	
 }
